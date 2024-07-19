45e5a1782a168b4b86257c9cd1d1a26d3438b80e
Alex Warda <alexsio03@gmail.com>
2024-07-18 21:58:14 -0700

    Initial Commit

---
+++ senior_project/senior_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  senior_projectApp.swift
+//  senior_project
+//
+//  Created by Alex Warda on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct senior_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
